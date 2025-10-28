5f4eda89c79c1fcbc92c31577f72989ab97a48a4
Rahul-Sundarraj <fftgosr@gmail.com>
2025-10-28 10:42:50 +0530

    fdfd

--- MailRemainderBackend/server.js
+++ MailRemainderBackend/server.js
@@ -13,11 +13,12 @@ app.use(express.urlencoded({ extended: true }))
 const scheduledMails={}
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  host: "smtp.sendgrid.net",
+  port: 587,
   auth: {
-        }
+    user: "apikey",
+    pass: process.env.SENDGRID_API_KEY
+  }
 });
 
 
